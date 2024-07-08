22a2adf6bfb9afdea7a7dd067924879c78683676
Syed N. Abbas <sayyadnaveedabbas@gmail.com>
2024-07-09 04:33:42 +0500

    Add project files.

---
+++ Running Balance in Crystal Report/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Running_Balance_in_Crystal_Report
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.ReportViewer = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.TBCustomerName = new System.Windows.Forms.TextBox();
+            this.TBDate = new System.Windows.Forms.TextBox();
+            this.TBNotes = new System.Windows.Forms.TextBox();
+            this.TBDueAmount = new System.Windows.Forms.TextBox();
+            this.TBCashReceived = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.TBCustomerName);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(865, 61);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnSave);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.TBCashReceived);
+            this.panel2.Controls.Add(this.TBDueAmount);
+            this.panel2.Controls.Add(this.TBNotes);
+            this.panel2.Controls.Add(this.TBDate);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 383);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(865, 134);
+            this.panel2.TabIndex = 1;
+            // 
+            // ReportViewer
+            // 
+            this.ReportViewer.ActiveViewIndex = -1;
+            this.ReportViewer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ReportViewer.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ReportViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ReportViewer.Location = new System.Drawing.Point(0, 61);
+            this.ReportViewer.Name = "ReportViewer";
+            this.ReportViewer.ShowGroupTreeButton = false;
+            this.ReportViewer.ShowLogo = false;
+            this.ReportViewer.ShowParameterPanelButton = false;
+            this.ReportViewer.ShowRefreshButton = false;
+            this.ReportViewer.Size = new System.Drawing.Size(865, 322);
+            this.ReportViewer.TabIndex = 2;
+            this.ReportViewer.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None;
+            // 
+            // TBCustomerName
+            // 
+            this.TBCustomerName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBCustomerName.Location = new System.Drawing.Point(152, 12);
+            this.TBCustomerName.Name = "TBCustomerName";
+            this.TBCustomerName.Size = new System.Drawing.Size(217, 26);
+            this.TBCustomerName.TabIndex = 0;
+            // 
+            // TBDate
+            // 
+            this.TBDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBDate.Location = new System.Drawing.Point(47, 32);
+            this.TBDate.Name = "TBDate";
+            this.TBDate.Size = new System.Drawing.Size(151, 26);
+            this.TBDate.TabIndex = 1;
+            // 
+            // TBNotes
+            // 
+            this.TBNotes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBNotes.Location = new System.Drawing.Point(219, 32);
+            this.TBNotes.Name = "TBNotes";
+            this.TBNotes.Size = new System.Drawing.Size(262, 26);
+            this.TBNotes.TabIndex = 2;
+            // 
+            // TBDueAmount
+            // 
+            this.TBDueAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBDueAmount.Location = new System.Drawing.Point(506, 32);
+            this.TBDueAmount.Name = "TBDueAmount";
+            this.TBDueAmount.Size = new System.Drawing.Size(142, 26);
+            this.TBDueAmount.TabIndex = 3;
+            // 
+            // TBCashReceived
+            // 
+            this.TBCashReceived.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBCashReceived.Location = new System.Drawing.Point(680, 32);
+            this.TBCashReceived.Name = "TBCashReceived";
+            this.TBCashReceived.Size = new System.Drawing.Size(142, 26);
+            this.TBCashReceived.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Customer Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(43, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Date:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(215, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Notes:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(502, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Due Amount:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(676, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(120, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Cash Received:";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(735, 82);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(86, 34);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(865, 517);
+            this.Controls.Add(this.ReportViewer);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer ReportViewer;
+        private System.Windows.Forms.TextBox TBCustomerName;
+        private System.Windows.Forms.TextBox TBCashReceived;
+        private System.Windows.Forms.TextBox TBDueAmount;
+        private System.Windows.Forms.TextBox TBNotes;
+        private System.Windows.Forms.TextBox TBDate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ Running Balance in Crystal Report/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Running_Balance_in_Crystal_Report
+{
+    public partial class Form1 : Form
+    {
+        string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Data\RunningBalance.mdf;Integrated Security=True";
+        public Form1()
+        {
+            InitializeComponent();
+            LoadReport();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if(TBCustomerName.Text.Trim().Length == 0 )
+            {
+                MessageBox.Show("Customer Name is Required");
+                TBCustomerName.Focus();
+                return;
+            }
+
+            string CustomerName = TBCustomerName.Text;
+
+            string DateStr = TBDate.Text;
+
+            try
+            {
+                Convert.ToDateTime(DateStr);
+            }
+            catch
+            {
+                MessageBox.Show("Invalid Date");
+                TBDate.Focus();
+                return;
+            }
+
+            string Notes = TBNotes.Text;
+
+            string DueAmountStr = TBDueAmount.Text;
+
+            try
+            {
+                if (!String.IsNullOrEmpty(DueAmountStr))
+                    Convert.ToDouble(DueAmountStr);
+            }
+            catch
+            {
+                MessageBox.Show("Invalid DueAmount");
+                TBDueAmount.Focus();
+                return;
+            }
+
+            string CashReceivedStr = TBCashReceived.Text;
+
+            try
+            {
+                if (!String.IsNullOrEmpty(CashReceivedStr))
+                    Convert.ToDouble(CashReceivedStr);
+            }
+            catch
+            {
+                MessageBox.Show("Invalid Cash Received");
+                TBCashReceived.Focus();
+                return;
+            }
+
+
+            if(DueAmountStr != "0")
+            {
+                string query = "Insert Into [Invoices] (CustomerName, Date, DueAmount, Notes) Values (@CustomerName, @Date, @DueAmount, @Notes)";
+                SqlConnection con = new SqlConnection(ConnectionString);
+                con.Open();
+                SqlCommand com = new SqlCommand(query, con);
+                com.Parameters.AddWithValue("@CustomerName", CustomerName);
+                com.Parameters.AddWithValue("@Date", DateStr);
+                com.Parameters.AddWithValue("@DueAmount", DueAmountStr);
+                com.Parameters.AddWithValue("@Notes", Notes);
+
+                com.ExecuteNonQuery();
+                con.Close();
+            }
+            else if(CashReceivedStr != "0")
+            {
+                string query = "Insert Into [Payments] (CustomerName, Date, PaidAmount, Notes) Values (@CustomerName, @Date, @PaidAmount, @Notes)";
+                SqlConnection con = new SqlConnection(ConnectionString);
+                con.Open();
+                SqlCommand com = new SqlCommand(query, con);
+                com.Parameters.AddWithValue("@CustomerName", CustomerName);
+                com.Parameters.AddWithValue("@Date", DateStr);
+                com.Parameters.AddWithValue("@PaidAmount", CashReceivedStr);
+                com.Parameters.AddWithValue("@Notes", Notes);
+
+                com.ExecuteNonQuery();
+                con.Close();
+            }
+
+            TBDate.Text = "";
+            TBNotes.Text = "";
+            TBDueAmount.Text = "";
+            TBCashReceived.Text = "";
+
+            LoadReport();
+        }
+
+        private void LoadReport()
+        {
+            ReportDS DS = new ReportDS();
+            RunningBalanceReport Report = new RunningBalanceReport();
+
+            DataRow row = DS.Tables["Report"].NewRow();
+            row["CustomerName"] = TBCustomerName.Text;
+            DS.Tables["Report"].Rows.Add(row);
+
+            SqlConnection con = new SqlConnection(ConnectionString);
+            con.Open();
+
+            string query = "Select Date, DueAmount As 'Debit', Notes, '0' As 'Credit' From [Invoices] Where CustomerName = @CustomerName" + Environment.NewLine +
+                "Union All" + Environment.NewLine +
+                "Select Date, '0' As Debit, Notes, (-1 * PaidAmount) As 'Credit' From [Payments] Where CustomerName = @CustomerName" + Environment.NewLine +
+                "Order By Date";
+
+            SqlCommand com = new SqlCommand(query, con);
+
+            com.Parameters.AddWithValue("@CustomerName", TBCustomerName.Text);
+
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            da.Fill(DS, "Rows");
+
+            Report.SetDataSource(DS);
+            ReportViewer.ReportSource = Report;
+        }
+    }
+}
